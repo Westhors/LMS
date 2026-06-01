5c919c4ab1b54b5917c5c790f3bc8faab7810bcb
ahmed abdullah <abdallah@pyramidsfreight.com>
2026-06-01 10:03:57 +0300

    s

--- app/Models/Admin.php
+++ app/Models/Admin.php
@@ -2,14 +2,13 @@
 
 namespace App\Models;
 
+use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use Illuminate\Database\Eloquent\Model;
 use Laravel\Sanctum\HasApiTokens;
 
-class Admin extends Model
+class Admin extends Authenticatable
 {
-    use HasApiTokens,HasFactory;
+    use HasApiTokens, HasFactory;
 
     protected $guarded = ['id'];
-
 }
