968ec0d34578b83c96ce539aeb7db255eb85676b
ahmed <ahmedabdallea94@gmail.com>
2026-06-24 12:41:53 +0300

    s

--- database/migrations/2026_04_22_123126_add_offer_fields_to_semesters_table.php
+++ database/migrations/2026_04_22_123126_add_offer_fields_to_semesters_table.php
@@ -11,14 +11,14 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('offers', function (Blueprint $table) {
+        Schema::table('semesters', function (Blueprint $table) {
             $table->unsignedBigInteger('offer_id')->nullable();
         });
     }
 
     public function down(): void
     {
-        Schema::table('offers', function (Blueprint $table) {
+        Schema::table('semesters', function (Blueprint $table) {
             $table->dropColumn([ 'offer_id']);
         });
     }
