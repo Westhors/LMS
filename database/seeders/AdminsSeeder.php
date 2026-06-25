bb39a3476c2707efada3d0c98d42e065f8d1be6d
ahmed <ahmedabdallea94@gmail.com>
2026-06-25 13:50:51 +0300

    2

---
+++ database/seeders/AdminsSeeder.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Admin;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\Hash;
+
+class AdminsSeeder extends Seeder
+{
+    public function run(): void
+    {
+        Admin::create([
+            'name' => 'Super Admin LMS',
+            'email' => 'Eslam@superadmin0716.com',
+            'password' => Hash::make('0123456789eslaM#%*$'),
+        ]);
+    }
+}
