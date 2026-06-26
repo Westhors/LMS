a5af9bd9f4c654d7a525fea012e19c5c70a5c207
ahmed <ahmedabdallea94@gmail.com>
2026-06-26 21:49:35 +0300

    ww

--- app/Models/Offer.php
+++ app/Models/Offer.php
@@ -2,13 +2,18 @@
 
 namespace App\Models;
 
+use App\Traits\HasMedia;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 
 class Offer extends Model
 {
     protected $guarded = ['id'];
+    use HasMedia;
 
+    protected $with = [
+        'media',
+    ];
     protected $casts = [
         'active' => 'boolean'
     ];
