6a7aeadd4ef2d24d31d8d6d98abc91568126da1a
HabibaSayed15 <habiba.sayed15.hs@gmail.com>
2026-07-07 22:14:49 +0300

    s

--- app/Http/Requests/StageRequest.php
+++ app/Http/Requests/StageRequest.php
@@ -22,8 +22,8 @@ class StageRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string',
-            'name_ar' => 'required|string',
+            'name' => 'nullable|string',
+            'name_ar' => 'nullable|string',
             'position' => 'nullable',
             'active' => 'boolean',
         ];
